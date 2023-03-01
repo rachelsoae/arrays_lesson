56a68842322bee578b9c9b656bfd3dd88a026915
Rachel Prather <rachelprather1@gmail.com>
2023-03-01 14:22:24 -0800

    Log a sentence explaining index positions

--- arrays.js
+++ arrays.js
@@ -1,10 +1,10 @@
 var familyMembers = ["Phil", "Lee", "Fay", "Meagan", "Rachel"]
 var familyMembersAges = [60, 65, 42, 36, 29]
-var booleans = [0 < 1, 0 === 0, 5 !== 10, 3 >= 4, 2 > 5]
+var trueOrFalse = [0 < 1, 0 === 0, 5 !== 10, 3 >= 4, 2 > 5]
 
 console.log(familyMembers)
 console.log(familyMembersAges)
-console.log(booleans)
+console.log(trueOrFalse)
 
 // The .shift() method removes the first item in the array
 familyMembers.shift()
@@ -14,5 +14,7 @@ console.log(familyMembers)
 console.log(familyMembersAges.slice(2, 6))
 
 // The .push() method adds a specified item to the end of an array
-booleans.push(5 !== 5)
-console.log(booleans)
+trueOrFalse.push(5 !== 5)
+console.log(trueOrFalse)
+
+console.log("Index positions start with 0, so even though we would consider Phil to be in first position, his index position is actually 0.")
